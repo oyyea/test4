db0034357d1e426e558a80787ca41809b23e381a
oyyea <463899552@qq.com>
2021-11-05 20:24:23 +0800

    作业

---
+++ src/test4_1/Circle.java
@@ -0,0 +1,25 @@
+package test4_1;
+
+public class Circle {
+	double radius;
+	
+	Circle(){
+		radius = 1;
+	}
+	
+	Circle(double newRadius){
+		radius = newRadius;
+	}
+	
+	double getArea() {
+		return radius*radius*Math.PI;
+	}
+	
+	double getPerimeter() {
+		return 2*radius*Math.PI;
+	}
+	
+	void setRadius(double newRadius) {
+		radius = newRadius;
+	}
+}
---
+++ src/test4_4/TotalArea.java
@@ -0,0 +1,41 @@
+package test4_4;
+
+public class TotalArea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Circle[] circleArray;
+		
+		circleArray = createCircleArray();
+		
+		printCircleArray(circleArray);
+	}
+	
+	public static Circle[] createCircleArray() {
+		Circle[] circleArray = new Circle[5];
+		
+		for(int i=0;i<circleArray.length;i++) {
+			circleArray[i] = new Circle(Math.random()*100);
+		}
+		
+		return circleArray;
+	}
+	
+	public static void 	printCircleArray(Circle[] circleArray) {
+		System.out.printf("%-30s%-15s\n", "Radius","Area");
+		for(int i=0;i<circleArray.length;i++) {
+			System.out.printf("%-30f%-15f\n",circleArray[i].getRadius(),
+				circleArray[i].getArea());
+		}
+		System.out.printf("%-30s%-15f\n","The total area of circle is",
+				sum(circleArray));
+	}
+	
+	public static double sum(Circle[] circleArray) {
+		double sum=0;
+		for(int i=0;i<circleArray.length;i++) {
+			sum+=circleArray[i].getArea();
+		}
+		return sum;
+	}
+}
